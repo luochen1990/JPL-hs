f28cae77c17913cb41b3c696795f79ccf35bb8b7
LuoChen <luochen1990@gmail.com>
2020-04-16 11:34:59 +0800

    backup: impl show for definitions

--- src/JPL/Core/Definitions.hs
+++ src/JPL/Core/Definitions.hs
@@ -1,6 +1,9 @@
 {-# language TupleSections #-}
 {-# language FlexibleInstances #-}
 
+{-# language RankNTypes #-}
+{-# language UndecidableInstances #-}
+
 -- | Providing core definitions about JsonSpec
 
 module JPL.Core.Definitions where
@@ -26,9 +29,21 @@ import Text.Pretty.Simple
 impossible :: a
 impossible = undefined
 
----------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- * ShowLiteral
+--------------------------------------------------------------------------------
+
+class ShowLiteral a where
+    -- | genLitWith showPart expr = (dispersity, str)
+    genLitWith :: (forall a'. ShowLiteral a' => Int -> a' -> String) -> a -> (Int, String)
+
+    showLit :: Int -> a -> String
+    showLit dispLimit expr = if disp > dispLimit then "(" ++ s ++ ")" else s where
+        (disp, s) = genLitWith showLit expr
+
+--------------------------------------------------------------------------------
 -- * Expr
----------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
 type Ident = String
 
@@ -49,7 +64,7 @@ data Expr =
     | Let Ident Expr Expr
     | Assume Expr Expr
     | Assert Expr Expr
-    deriving (Show, Eq)
+    deriving (Eq)
 
 data Pattern =
       ConstNullPat
@@ -59,11 +74,49 @@ data Pattern =
     | DictPat (Map String Pattern)
     | ListPat [Pattern]
     | VarPat Ident
-    deriving (Show, Eq)
+    deriving (Eq)
 
---------------------------------------------------------------------------------
+-- ** show instances
+
+instance ShowLiteral Expr where
+    genLitWith showPart expr = case expr of
+        Null -> (0, "null")
+        Number x -> (0, show x) --TODO: specify format
+        Text s -> (0, show s) --TODO: escape
+        Boolean b -> (0, if b then "true" else "false")
+        Dict mp -> (0, "{" ++ intercalate ", " [showKey k ++ ": " ++ showPart 1 v | (k, v) <- M.toList mp] ++ "}")
+        List xs -> (0, "[" ++ intercalate ", " [showPart 1 x | x <- xs] ++ "]")
+        Var id -> (0, id)
+        App ef ex -> (1, showPart 1 ef ++ " " ++ showPart 0 ex)
+        Let k v e -> (2, k ++ " := " ++ showPart 1 v ++ "; " ++ showPart 2 e)
+        Assume p e -> (2, "assume " ++ showPart 1 p ++ "; " ++ showPart 2 e)
+        Assert p e -> (2, "assert " ++ showPart 1 p ++ "; " ++ showPart 2 e)
+        Case ex ps -> (3, "case " ++ showPart 1 ex ++ " of " ++ intercalate ", " [showPart 1 p ++ ": " ++ showPart 1 e | (p, e) <- ps])
+        Lam id e -> (4, id ++ "? " ++ showPart 4 e)
+        where
+            showKey k = if True then k else show k --TODO: escape
+
+instance ShowLiteral Pattern where
+    genLitWith showPart expr = case expr of
+        ConstNullPat -> (0, "null")
+        ConstNumberPat x -> (0, show x) --TODO
+        ConstTextPat s -> (0, show s) --TODO
+        ConstBooleanPat b -> (0, if b then "true" else "false")
+        DictPat mp -> (0, "{" ++ intercalate "," [showKey k ++ ":" ++ showPart 1 v | (k, v) <- M.toList mp] ++ "}")
+        ListPat xs -> (0, "[" ++ intercalate "," [showPart 1 x | x <- xs] ++ "]")
+        VarPat id -> (0, id)
+        where
+            showKey k = if True then k else show k --TODO: escape
+
+instance Show Expr where
+    show expr = "Expr `" ++ (showLit maxBound expr) ++ "`"
+
+instance Show Pattern where
+    show pat = "Pattern `" ++ (showLit maxBound pat) ++ "`"
+
+-------------------------------------------------------------------------------------------
 -- * EvalResult
---------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------
 
 data EvalResult a =
       Success a
